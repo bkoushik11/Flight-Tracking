13eca35a24de6492309a6a123fb02b8bdf980f0f
bkoushik11 <koushikbevanapally@gmail.com>
2025-09-09 12:48:28 +0530

    updated frontend

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import { FlightMap } from './components/FlightMap';
 import { FilterPanel } from './components/FilterPanel';
 import { AlertsPanel } from './components/AlertsPanel';
